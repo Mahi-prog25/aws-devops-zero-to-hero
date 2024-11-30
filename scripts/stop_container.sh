48ddc98552f76feab52c98cf50b0431292c542c1
Mahi-prog25 <madhavimahankali25@gmail.com>
2024-11-30 15:48:58 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 set -e
 
-# Stop the running container based on the image
-CONTAINER_ID=$(docker ps -q --filter ancestor=mahankali2k/simple-python-flask-app)
-
-if [ -n "$CONTAINER_ID" ]; then
-  echo "Stopping container with ID: $CONTAINER_ID"
-  docker stop "$CONTAINER_ID"
-  docker rm "$CONTAINER_ID"
-else
-  echo "No container running with the specified image."
-fi
+# Stop the running container (if any)
+echo "Hi"
